39872edad4cc922664c2937a5dd469f8d698ee03
zeldon-0 <48602270+zeldon-0@users.noreply.github.com>
2020-10-05 17:58:38 +0300

    Add seeding for all entities

--- AOIS1.BackEnd/AOIS1.Data/DB Seeding/SeedArtistNovelties.sql
+++ AOIS1.BackEnd/AOIS1.Data/DB Seeding/SeedArtistNovelties.sql
@@ -1,12 +1,26 @@
 use [ExpertSystemDB];
 
 DELETE 
-FROM [Tempos];
+FROM [ArtistNovelty];
 
 
-INSERT INTO [Tempos] (Name) VALUES
-	('Very slow'),
-	('Slow'),
-	('Moderate'),
-	('Fast'),
-	('Very fast');
+INSERT INTO [ArtistNovelty] (ArtistId, NoveltyId, Probability) VALUES
+	(11, 3, 1),
+	(12, 2, 0.8),
+	(12, 3, 0.7),
+	(13, 1, 0.6),
+	(13, 2, 1),
+	(13, 3, 0.5),
+	(14, 2, 1),
+	(14, 3, 0.6),
+	(15, 1, 0.3),
+	(15, 2, 0.7),
+	(16, 3, 1),
+	(17, 2, 0.5),
+	(17, 3, 0.9),
+	(18, 2, 0.8),
+	(18, 3, 0.7),
+	(19, 2, 0.4),
+	(19, 3, 0.85),
+	(20, 3, 1),
+	(21, 2, 0.95);
